27e8e98d70c6701a3286776e47cc65900f80e528
ilya41299 <32190925+ilya41299@users.noreply.github.com>
2022-04-13 23:13:34 +0300

    Update lab02.py

--- lab02.py
+++ lab02.py
@@ -1,9 +1,18 @@
 import numpy as np
 import lab01
 
+
+def analysis_method():
+    a, b, c, d, e = -5, 9 / 2, 15, -9 / 2, -9
+
+    x = (c * e - 2 * b * d) / (4 * a * b - c ** 2)
+    y = (-c * x - e) / (2 * b)
+    print(f"Аналитическое решение: H(x,y)= {H(x, y)}")
+
+
 def H(x, y):
-#    result = -5 * x ** 2 + (9 / 2) * y ** 2 + 15 * x * y + (-9 / 2) * x + (-9) * y
-    return -3 * x ** 2 + (3 / 2) * y ** 2 + (18/5) * x * y + (-18 / 50) * x + (-72/25) * y
+    return -5 * x ** 2 + (9 / 2) * y ** 2 + 15 * x * y + (-9 / 2) * x + (-9) * y
+    # return -3 * x ** 2 + (3 / 2) * y ** 2 + (18 / 5) * x * y + (-18 / 50) * x + (-72 / 25) * y
 
 
 def kernel(N):
@@ -29,20 +38,27 @@ def saddle_point(matrix):
 
 def compute():
     N = 2
-    while N <= 10:
+    H_queue = []
+    while N <= 7 or len(set(H_queue)) != 1:
         matrix = kernel(N)
         i, j, saddle_value = saddle_point(matrix)
         if i == -1:
             i, j, *_ = lab01.braun_robinson(matrix)
             saddle_value = matrix[i][j]
+        # print(matrix)
+        for row in range(0, len(matrix)):
+            for column in range(0, len(matrix)):
+                matrix[row][column] = round(matrix[row][column], 2)
+        print(f"N={N}")
         print(matrix)
-        print(f'i = {i}, j = {j}, H = {saddle_value}')
+        print(f'x = {round(i / N, 3)}, y = {round(j / N, 2)}, H = {round(saddle_value, 3)}\n')
 
-        N+=1
+        N += 1
+        H_queue.append(round(saddle_value, 3))
+        if len(H_queue) > 5:
+            H_queue.pop(0)
 
 
+analysis_method()
+print(f"Численное решение:")
 compute()
-
-
-
-
