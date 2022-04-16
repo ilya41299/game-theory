8bb52e52ff828a96924c0fb5aa4f94e191dfb2a6
ilya41299 <32190925+ilya41299@users.noreply.github.com>
2022-04-17 00:08:07 +0300

    Update lab03.py

--- lab03.py
+++ lab03.py
@@ -1,84 +1,39 @@
 import numpy as np
-import random
 
-np.set_printoptions(precision=3, floatmode='fixed')
+Matrix_A = np.array(
+    [[24, 2, 40, -46, 27, -46, 35, -23, -33, -50],
+     [-40, 28, -15, 38, 26, -38, -35, -26, -37, -19],
+     [-1, -36, 30, -35, 4, 7, 17, -6, -8, -33],
+     [46, 1, 2, -48, 45, -3, -29, -24, 36, 24],
+     [-9, 41, -9, -17, -30, -20, -44, 41, -43, 9],
+     [19, 49, 0, -30, -48, 38, -18, -27, 35, 11],
+     [37, 4, -9, -7, -21, -9, 15, -49, -23, -20],
+     [-33, 29, -29, 24, -21, 17, 49, 41, -36, 40],
+     [-40, 24, -45, 49, 1, 47, 49, -17, 26, -15],
+     [46, 9, 28, -38, -11, 32, 9, 42, 19, 37]])
 
+Matrix_B = np.array([[32, 2, 17, -29, -42, -8, -20, -16, 12, -1],
+                     [-11, -22, -47, 49, -25, 26, 35, 30, 23, -30],
+                     [46, -14, -17, 5, -22, -33, -2, -15, -48, -14],
+                     [28, -6, 18, -38, 49, -28, 37, 7, 17, -19],
+                     [25, -48, 6, -22, 12, -5, -32, -35, 42, -33],
+                     [13, -37, -10, 39, -16, 42, -31, -6, -16, -43],
+                     [10, -38, 36, -30, 5, -9, -30, 34, 43, -15],
+                     [37, 23, 5, 17, 49, -30, 47, 8, 6, 34],
+                     [9, -32, 41, 34, -12, 43, -11, -39, 24, -3],
+                     [-49, -5, 36, 24, -39, -41, -13, 10, -18, 4]])
 
-def init_matrix(n, min, max):
-    return np.random.randint(min, max, (n, n))
+zk_a = np.array([
+    [-5, 0],
+    [-10, -1]
+])
+zk_b = np.array([
+    [-5, -10],
+    [0, -1]
+])
 
+crossroads_a = np.array([[1, 1 - 0.1], [2, 0]])
+crossroads_b = np.array([[1, 2], [1 - 0.1, 0]])
 
-def nash(matrix_a, matrix_b):
-    # column_max_a = np.argmax(matrix_a, axis=0)
-    # row_max_b = np.argmax(matrix_b, axis=1)
-    row_max_b = [np.where(row == row.max())[0] for row in matrix_b]
-    column_max_a = [np.where(column == column.max())[0] for column in matrix_a.T]
-    priemlemo_a = []
-    priemlemo_b = []
-    for i in range(len(column_max_a)):
-        for j in range(len(column_max_a[i])):
-            priemlemo_a.append((column_max_a[i][j] + 1, i + 1))
-    for i in range(len(row_max_b)):
-        for j in range(len(row_max_b[i])):
-            priemlemo_b.append((i + 1, row_max_b[i][j] + 1))
-    c = list(set(priemlemo_a) & set(priemlemo_a))
-    return c
-
-
-def is_pareto_effective(row, column, matrix_a, matrix_b):
-    n=len(matrix_a)
-    bool_a = np.empty(shape=(n,n), dtype=bool)
-    bool_b = np.empty(shape=(n,n), dtype=bool)
-    for i in range(n):
-        for j in range(n):
-            bool_a[i][j] = matrix_a[i][j] >= matrix_a[row][column]
-            bool_b[i][j] = matrix_b[i][j] >= matrix_b[row][column]
-            if bool_a[i][j] and bool_b[i][j]:
-                if matrix_a[i][j] > matrix_a[row][column] or matrix_b[i][j] > matrix_b[row][column]:
-                    return False
-    return True
-
-
-def pareto(matrix_a, matrix_b):
-    paretto_effective = []
-    for i in range(len(matrix_a)):
-        for j in range(len(matrix_a)):
-            if is_pareto_effective(i, j, matrix_a, matrix_b):
-                paretto_effective.append((i + 1, j + 1))
-    return paretto_effective
-
-
-def print_result(a, b, nash_effective, paretto_effective):
-    print()
-    print('A:')
-    for el in a:
-        print(el, sep='\n')
-    print()
-    print('B:')
-    for el in b:
-        print(el, sep='\n')
-    print()
-    print("Эффективно по Нэшу:")
-    print(", ".join(repr(e) for e in nash_effective))
-    print()
-    print("Парето эффективны:")
-    print(", ".join(repr(e) for e in paretto_effective))
-
-
-if __name__ == "__main__":
-    # a, b = init_matrix()
-    a, b = init_matrix(10, -50, 50), init_matrix(10, -50, 50)
-
-    nash_effective = nash(a, b)
-    paretto_effective = pareto(a, b)
-    print_result(a, b, nash_effective, paretto_effective)
-
-    # a = np.array([
-    #     [1, 4, 7],
-    #     [2, 0, 0],
-    #     [2, 0, 0],
-    # ]
-    # )
-    # b = np.array([[0, 0, 0],
-    #               [5, 1, 2],
-    #               [5, 3, 5]])
+family_dispute_a = np.array([[4, 0], [0, 1]])
+family_dispute_b = np.array([[1, 0], [0, 4]])
